6bfbc315d1ab8f0a7ea9d506bb6fe76f2e4ba033
Rachel Starosta <raychelstarr21@gmail.com>
2023-06-07 08:55:54 -0400

    first commit -m no functionality

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          What is up my dudes
+        
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
