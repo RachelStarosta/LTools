3418b71556e97a4af9e038c04d838be15f873469
Rachel Starosta <raychelstarr21@gmail.com>
2023-06-09 11:30:24 -0400

    help from chatgpt
    still bugs

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 
-const menu = document.getElementById("menu");
 
-Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover = () => {
-      menu.dataset.activeIndex = index;
-    }
-  });
-  
+
 const HomePage = () => {
-
+    useEffect(() => {
+        const menu = document.getElementById("menu");
+    
+        const handleMouseMove = (event) => {
+          const mouseX = event.clientX - menu.getBoundingClientRect().left; //x axis
+          const index = Math.floor(mouseX / (menu.offsetWidth / 4)); 
+    
+          console.log("mouseX:" + mouseX  + "index:"+  index); // Output the index to the console
+        };
+    
+        menu.addEventListener("mousemove", handleMouseMove);
+    
+        // Clean up the event listener when the component is unmounted
+        return () => {
+          menu.removeEventListener("mousemove", handleMouseMove);
+        };
+      }, []);
     return (
         <>
-        <div id = "menu">
+        <div id = "menu" data-active-index>
             <div id = "menu-items"> 
             <a href = "/" className = "menu-item"> Home</a>
             <a href = "/" className = "menu-item"> Gallery</a>
--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const menu = document.getElementById("menu");
-
-Array.from(document.getElementsByClassName("menu-item"))
-  .forEach((item, index) => {
-    item.onmouseover = () => {
-      menu.dataset.activeIndex = index;
-    }
-  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <menu/>
+   
   </React.StrictMode>
 );
 
